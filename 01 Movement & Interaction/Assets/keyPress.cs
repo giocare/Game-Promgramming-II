40afd5f80bb0cc8eca2e91e2b0780537368d93b7
giocare <jessica.adjei102@gmail.com>
2019-01-31 21:40:12 -0500

    Added Scripts

---
+++ 01 Movement & Interaction/Assets/keyPress.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyPress : MonoBehaviour
+{
+    public float moveSpeed;
+    public float turnSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5;
+        turnSpeed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+         {
+            // transform.Translate(new Vector3(0, -1, 0));
+            transform.Translate(Vector3.forward);
+         }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, -1, 0)*turnSpeed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, 1, 0)*turnSpeed);
+        }
+
+
+        // transform.Translate(new Vector3(0, 0, moveSpeed)*Time.deltaTime);
+    }
+
+    /* void OnMouseDown()
+     {
+         //print to console
+        Debug.Log("Clicked!");
+
+         // move up one when clicked
+         transform.Translate(new Vector3(0,1,0));
+     }
+     */
+}
