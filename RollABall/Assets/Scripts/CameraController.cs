2ca362a60dd9220767f35ee3d6b3297f6572c03d
giocare <jessica.adjei102@gmail.com>
2019-02-06 22:56:07 -0500

    RollABall Tutorial Complete

---
+++ RollABall/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // calculating the difference  between the position of the camera and the player
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate waits until everything has been executed
+    void LateUpdate()
+    {
+        // update position of camera
+        transform.position = player.transform.position + offset;
+    }
+}
